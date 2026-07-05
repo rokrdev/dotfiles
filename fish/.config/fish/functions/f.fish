9a1d520b33c7f6f165602821154d821526f736f5
rokr-dev <bharatjoshi.p@gmail.com>
2026-07-05 12:04:58 +1000

    zellij: update keybindings

--- fish/.config/fish/functions/f.fish
+++ fish/.config/fish/functions/f.fish
@@ -1,5 +1,5 @@
 function f --description 'Fuzzy find file and open in vim'
-    set file (echo (eval "$FZF_DEFAULT_COMMAND | fzf --exit-0 --preview 'bat --style=plain --color=always --line-range :100 {}'"))
+    set file (echo (eval "$FZF_DEFAULT_COMMAND | fzf --popup 80% --exit-0 --preview 'bat --style=plain --color=always --line-range :100 {}'"))
     if test -n $file
         $EDITOR (echo $file)
     end

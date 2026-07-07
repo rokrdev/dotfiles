49d297ecc9098d0238d2c5810bb8a179f340e893
rokr-dev <bharatjoshi.p@gmail.com>
2026-07-07 23:45:44 +1000

    feat(fish): split fish config into multiple conf.d files
    files in conf.d are sourced first and then config.fish is sourced. This is the right way to load fish config
    This also splits the large config.fish into proper smaller conf.d files.

---
+++ fish/.config/fish/conf.d/tools.fish
@@ -0,0 +1,13 @@
+# Docker initialization
+if test -e ~/.docker/init-fish.sh
+    source $HOME/.docker/init-fish.sh || true # Added by Docker Desktop
+end
+
+# Set color scheme
+setscheme tokyonight_night
+
+# FZF initialization
+fzf --fish | source
+
+# Zoxide initialization
+zoxide init fish | source

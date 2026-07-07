0ed785f75f2f9055b446216f4d287a1a1340fd2f
rokr-dev <bharatjoshi.p@gmail.com>
2026-07-08 09:37:17 +1000

    feat(fish): update aliases and abbreviations

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -20,33 +20,21 @@ alias kl="kubectl logs"
 alias klf="kubectl logs -f"
 alias lg=lazygit
 alias n=npm
-alias ni='npm install'
 alias rf=trash
 alias sf='source $FISH_CONFIG'
 alias st=speedtest-cli
 alias top=btop
 alias upa='brew update && brew upgrade && brew upgrade --cask && brew cleanup && brew doctor; nvim +PackerSync +PackerCompile +qall > /dev/null'
-alias ya='yarn add'
-alias yr='yarn run'
-alias ys='yarn start'
-alias yt='yarn test'
 
 # Changing "ls" to "eza"
 # alias ls='eza --color=always --group-directories-first'  # all files and dirs
 
-alias please=sudo
-
 alias python3=python
 alias p=python
-
-alias x86="env /usr/bin/arch -x86_64 /bin/zsh --login"
 
 alias clb="claude --model $CLAUDE_MODEL_SONNET --effort high"
 alias cld="claude --model $CLAUDE_MODEL_OPUS --effort high"
 
 #provider/model
-alias ocq="opencode -m lmstudio/qwen3.6-27b-mlx"
-alias ocg="opencode -m lmstudio/gemma-4-31b"
-
-alias ldeep='fish $HOME/.dotfiles/late/late_switch.fish deep'
-alias llite='fish $HOME/.dotfiles/late/late_switch.fish lite'
+# alias ocq="opencode -m lmstudio/qwen3.6-27b-mlx"
+# alias ocg="opencode -m lmstudio/gemma-4-31b"

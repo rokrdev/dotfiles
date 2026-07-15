39022c3b69ccf05c267f8d1b7751387fd17ad023
rokrdev <bharatjoshi.p@gmail.com>
2026-07-16 08:53:02 +1000

    chore(fish): add Linuxbrew path for Linux systems
    Conditionally add /home/linuxbrew/.linuxbrew/bin to PATH when
    running on Linux, enabling Linuxbrew package manager integration.

--- fish/.config/fish/conf.d/00-env.fish
+++ fish/.config/fish/conf.d/00-env.fish
@@ -8,15 +8,7 @@ export LANG="en_US.UTF-8"
 export COLORTERM=truecolor
 ssh-add -l &>/dev/null || ssh-add -A &>/dev/null
 
-set -gx GNUPGHOME "$HOME/.asdf/keyrings/nodejs"
-if not test -d "$GNUPGHOME"
-    mkdir -p "$GNUPGHOME"
-end
-chmod 0700 "$GNUPGHOME"
-
-export ANDROID_HOME="$HOME/Library/Android/sdk"
-export ANDROID_SDK_ROOT="$ANDROID_HOME/sdk"
-export GOPATH="$HOME/go"
+export GOPATH="$HOME/.go"
 export GOBIN="$GOPATH/bin"
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow -S'
 export FZF_CTRL_R_OPTS='--sort --exact'
@@ -24,6 +16,10 @@ export FZF_DEFAULT_OPTS='--popup 80% --bind=ctrl-a:select-all,ctrl-d:deselect-al
 export _ZO_FZF_OPTS='--popup 80%'
 
 # setp yarn prefix first with this `yarn config set prefix "~/.yarn/"`
-export PATH="$HOME/.fzf/bin:$HOME/.cargo/bin:/snap/bin:$HOME/.docker/bin:$HOME/bin:$HOME/.bun/bin:$GOPATH/bin:$ANDROID_SDK_ROOT:$HOME/.yarn/bin:$HOME/.local/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+export PATH="$HOME/.cargo/bin:$HOME/.docker/bin:$HOME/bin:$HOME/.bun/bin:$GOPATH/bin:$ANDROID_SDK_ROOT:$HOME/.yarn/bin:$HOME/.local/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+if test (uname) = "Linux"
+    set -g PATH "/home/linuxbrew/.linuxbrew/bin" $PATH
+end
 
 export XDG_CONFIG_HOME="$HOME/.config"

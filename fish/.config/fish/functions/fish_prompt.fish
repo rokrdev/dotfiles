e381d90273091b6f00f3a23e6e6bac8aa00414de
rokrdev <6837979+rokrdev@users.noreply.github.com>
2026-07-15 21:54:45 +1000

    chore: switch from TokyoNight to Catppuccin Mocha theme across all tools
    Replaces the TokyoNight theme with Catppuccin Mocha in Fish, Helix, Ghostty,
    GitUI, Yazi, Herdr, and Bat. Updates the Fish prompt to remove hardcoded
    TokyoNight color variables and adds Helix custom theme and Yazi package files.
    
    Assisted-by: Crush:xiaomi/mimo-v2.5

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,37 +1,3 @@
-# TokyoNight Color Palette
-set -g foreground c0caf5
-set -g selection 33467C
-set -g comment 565f89
-set -g red f7768e
-set -g orange ff9e64
-set -g yellow e0af68
-set -g green 9ece6a
-set -g purple 9d7cd8
-set -g cyan 7dcfff
-set -g pink bb9af7
-
-# Syntax Highlighting Colors
-set -g fish_color_normal $foreground
-set -g fish_color_command $cyan
-set -g fish_color_keyword $pink
-set -g fish_color_quote $yellow
-set -g fish_color_redirection $foreground
-set -g fish_color_end $orange
-set -g fish_color_error $red
-set -g fish_color_param $purple
-set -g fish_color_comment $comment
-set -g fish_color_selection --background=$selection
-set -g fish_color_search_match --background=$selection
-set -g fish_color_operator $green
-set -g fish_color_escape $pink
-set -g fish_color_autosuggestion $comment
-
-# Completion Pager Colors
-set -g fish_pager_color_progress $comment
-set -g fish_pager_color_prefix $cyan
-set -g fish_pager_color_completion $foreground
-set -g fish_pager_color_description $comment
-
 set __fish_git_prompt_show_informative_status true
 set __fish_git_prompt_showcolorhints yes
 set __fish_git_prompt_showupstream informative
@@ -45,8 +11,8 @@ set __fish_git_prompt_color_upstream cyan
 set __fish_git_prompt_color_branch magenta
 set -U fish_prompt_pwd_dir_length 0
 
-set -U fish_color_cwd blue
-set -U fish_color_cwd_root red
+# set -U fish_color_cwd blue
+# set -U fish_color_cwd_root red
 
 function fish_prompt --description 'Write out the prompt'
     set -l _display_status $status
@@ -56,18 +22,18 @@ function fish_prompt --description 'Write out the prompt'
     # set -l prompt '⟫ '
     set -l prompt '󰘩 '
 
-    set -l prompt_color $red
+    set -l prompt_color red
 
     if test $_display_status -eq 0
-        set prompt_color $green
+        set prompt_color green
     end
 
     set -l pwd (prompt_pwd)
 
-    set -l hermit ''
-    if test -n "$HERMIT_ENV"
-        set hermit '🐚 '
-    end
+    # set -l hermit ''
+    # if test -n "$HERMIT_ENV"
+    #     set hermit '🐚 '
+    # end
 
     echo -n -s -e (set_color $fish_color_cwd) $pwd $git $hermit(set_color $purple) ' ' (date +%H:%M:%S) '\n' (set_color $prompt_color) $prompt
 end

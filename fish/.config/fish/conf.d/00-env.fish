5fa78e372b931c8a8dfdcb71fe827219399a11e9
rokrdev <rokrdev@proton.me>
2026-08-04 10:36:42 +1000

    fish: add catppuccin colors to fzf, use nerdfont glyphs for prompt mode indicators

--- fish/.config/fish/conf.d/00-env.fish
+++ fish/.config/fish/conf.d/00-env.fish
@@ -12,13 +12,19 @@ export GOPATH="$HOME/.go"
 export GOBIN="$GOPATH/bin"
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow -S'
 export FZF_CTRL_R_OPTS='--sort --exact'
-export FZF_DEFAULT_OPTS='--popup 80% --bind=ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all'
+export FZF_DEFAULT_OPTS="\
+--bind=ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all \
+--color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
+--color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
+--color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
+--color=selected-bg:#45475A \
+--color=border:#6C7086,label:#CDD6F4"
 export _ZO_FZF_OPTS='--popup 80%'
 
 # setp yarn prefix first with this `yarn config set prefix "~/.yarn/"`
 export PATH="$HOME/.cargo/bin:$HOME/.docker/bin:$HOME/bin:$HOME/.bun/bin:$GOPATH/bin:$ANDROID_SDK_ROOT:$HOME/.yarn/bin:$HOME/.local/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 
-if test (uname) = "Linux"
+if test (uname) = Linux
     set -g PATH "/home/linuxbrew/.linuxbrew/bin" $PATH
 end
 

d210a1a70d25ce1a7b9ae3be5af95431a7c2e985
rokrdev <rokrdev@proton.me>
2026-08-06 12:43:05 +1000

    tune fish/helix/herdr/moxide configs
    fish: use fzf default opts for zoxide with reverse layout popup
    helix: drop jdtls java config, simplify java roots
    herdr: hide pane scrollbars, move tab bar to top
    moxide: fix inbox folder name, move daily notes to Journal/Daily

--- fish/.config/fish/conf.d/00-env.fish
+++ fish/.config/fish/conf.d/00-env.fish
@@ -13,13 +13,15 @@ export GOBIN="$GOPATH/bin"
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow -S'
 export FZF_CTRL_R_OPTS='--sort --exact'
 export FZF_DEFAULT_OPTS="\
+--layout=reverse \
+--height=75% \
 --bind=ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all \
 --color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
 --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
 --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
 --color=selected-bg:#45475A \
 --color=border:#6C7086,label:#CDD6F4"
-export _ZO_FZF_OPTS='--popup 80%'
+export _ZO_FZF_OPTS=$FZF_DEFAULT_OPTS
 
 # setp yarn prefix first with this `yarn config set prefix "~/.yarn/"`
 export PATH="$HOME/.cargo/bin:$HOME/.docker/bin:$HOME/bin:$HOME/.bun/bin:$GOPATH/bin:$ANDROID_SDK_ROOT:$HOME/.yarn/bin:$HOME/.local/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"

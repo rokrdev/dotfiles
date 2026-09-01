697214c3de7f8bb903a65bf75b89fcde182fecc6
rokrdev <rokrdev@proton.me>
2026-09-01 15:01:48 +1000

    sketchybar: set normal for topmost
    when a window renders under sketchybar, it is not clickable or resizable. This fixes it but not sure what other sideeffects are there.
    Evaluating for now

--- sketchybar/.config/sketchybar/bar.lua
+++ sketchybar/.config/sketchybar/bar.lua
@@ -2,12 +2,12 @@ local colors = require("colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-	topmost = "window",
-	height = 40,
-	color = colors.transparent,
-	padding_right = 2,
-	padding_left = 2,
-	margin = 10,
-	shadow = true,
-	blur_radius = 10,
+  topmost = "normal",
+  height = 40,
+  color = colors.transparent,
+  padding_right = 2,
+  padding_left = 2,
+  margin = 10,
+  shadow = true,
+  blur_radius = 10,
 })
